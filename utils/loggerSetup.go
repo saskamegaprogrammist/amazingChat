7a8852fe76f34fecce79b89a5958759d9c138860
saskamegaprogrammist <spiridonovaalex@gmail.com>
2020-08-22 01:37:12 +0300

    [ADD] Dockerfile

--- utils/loggerSetup.go
+++ utils/loggerSetup.go
@@ -5,7 +5,7 @@ import (
 	"os"
 )
 
-const verbose = true
+const verbose = false
 
 var loggerFile *os.File
 
@@ -13,7 +13,7 @@ func LoggerSetup() {
 	var err error
 	loggerFile, err = os.OpenFile(LogFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
 	if err != nil {
-		logger.Fatalf("Faigled to open log file: %v", err)
+		logger.Fatalf("Failed to open log file: %v", err)
 	}
 	logger.Init("LoggerExample", verbose, true, loggerFile)
 }
